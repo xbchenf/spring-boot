6ab27daa140b76cb795074833580bf4a8747521a
xbchenf <xiaobing259@163.com>
2018-12-17 15:44:04 +0800

    update

--- sc09-springbcloud-springsession-redis/src/main/java/com/xbchen/springcloud/config/RedisCacheConfig.java
+++ sc09-springbcloud-springsession-redis/src/main/java/com/xbchen/springcloud/config/RedisCacheConfig.java
@@ -24,7 +24,7 @@ import java.util.concurrent.ConcurrentHashMap;
 @EnableCaching
 public class RedisCacheConfig  extends CachingConfigurerSupport {
     Logger logger = LoggerFactory.getLogger(RedisCacheConfig.class);
-    @Value("${spring.redis.host}")
+    /*@Value("${spring.redis.host}")
     private String host;
 
     @Value("${spring.redis.port}")
@@ -52,13 +52,40 @@ public class RedisCacheConfig  extends CachingConfigurerSupport {
 
         JedisPool jedisPool = new JedisPool(jedisPoolConfig, host, port, timeout, password);
         return jedisPool;
+    }*/
+    /**
+     * @Bean 和 @ConfigurationProperties
+     * 该功能在官方文档是没有提到的，我们可以把@ConfigurationProperties和@Bean和在一起使用。
+     * 举个例子，我们需要用@Bean配置一个Config对象，Config对象有a，b，c成员变量需要配置，
+     * 那么我们只要在yml或properties中定义了a=1,b=2,c=3，
+     * 然后通过@ConfigurationProperties就能把值注入进Config对象中
+     * @return
+     **/
+    @Bean
+    @ConfigurationProperties(prefix = "spring.redis.pool")
+    public JedisPoolConfig getRedisConfig() {
+        JedisPoolConfig config = new JedisPoolConfig();
+        return config;
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.redis")
+    public JedisConnectionFactory getConnectionFactory() {
+        JedisConnectionFactory factory = new JedisConnectionFactory();
+        factory.setUsePool(true);
+        JedisPoolConfig config = getRedisConfig();
+        factory.setPoolConfig(config);
+        logger.info("JedisConnectionFactory bean init success.");
+        return factory;
     }
 
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory cf) {
+    public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
-        redisTemplate.setConnectionFactory(cf);
+        JedisConnectionFactory factory = getConnectionFactory();
+        logger.info("JedisConnectionFactory >>>host:"+factory.getHostName()+";port:"+factory.getPort()+";password"+factory.getPassword());
+        redisTemplate.setConnectionFactory(factory);
         return redisTemplate;
     }
 
